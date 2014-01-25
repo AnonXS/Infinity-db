4dde136c298669a4c71e2421ddf8f3757831cff1
LordPsyan <realmsofwarcraft@gmail.com>
2014-01-25 11:57:34 -0500

    command table update. removed placeholder.

---
+++ Updates/world/00001-2014_01_25_01-world.command.sql
@@ -0,0 +1,4 @@
+DELETE FROM `command` WHERE `name` = 'modify runicpower';
+DELETE FROM `command` WHERE `name` = 'npc set phase';
+DELETE FROM `command` WHERE `name` = 'reload all achievement';
+DELETE FROM `command` WHERE `name` = 'debug transport';
